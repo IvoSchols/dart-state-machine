fccc0eef70304e85641561d49c8bdb7bd53eb86c
Ivo <ivoschols97@gmail.com>
2022-08-30 11:24:28 +0200

    added import

--- lib/state_machine.dart
+++ lib/state_machine.dart
@@ -3,6 +3,7 @@ library state_machine;
 import 'dart:collection';
 
 import 'package:binary_expression_tree/binary_expression_tree.dart';
+import 'package:equatable/equatable.dart';
 
 part 'src/state_machine.dart';
 part 'src/state.dart';
